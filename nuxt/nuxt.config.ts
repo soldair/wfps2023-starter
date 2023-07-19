d641666233ff2c83605b5407dfd2efcca816adbe
Ryan Day <ryaneday@google.com>
2023-07-19 16:35:12 -0700

    adding login page with client id
    cleaning up double prerender snippet from config

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -15,11 +15,6 @@ function loadGithubAppKey() {
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    prerender: {
-      routes: ['/about'],
-    },
-  },
   devtools: { enabled: true },
   nitro: {
     prerender: {
---
+++ nuxt/pages/login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  let data = "";
+  if(process.server){
+    const config = useRuntimeConfig();
+    data = config.github.appConfig.oauth.clientId;
+  }
+</script>
+<template>
+    <section>
+    <hr/>
+    <a :href="`https://github.com/login/oauth/authorize?client_id=${data}`">Login with GitHub</a>
+    <hr/>
+    </section>
+</template>
