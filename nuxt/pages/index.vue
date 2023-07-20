9bfb00a1fa089da8b1197ba0fcfc8900b2817b00
Samuel Arcidiacono <djjeck@google.com>
2023-07-20 22:05:25 +0000

    Style homepage

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -3,19 +3,41 @@ import type { DiscussionListResponse } from "../server/api/discussions";
 
 const { data } = await useFetch<DiscussionListResponse>("/api/discussions");
 </script>
+<style>
+.discussions {
+  padding: 10px;
+  max-width: 900px;
+  margin: 0 auto;
+  background-color: #fff;
+  border-radius: 10px;
+}
+
+.discussions h2 {
+  font-size: 1.25em;
+}
+
+.discussions h2 a {
+  text-decoration: none;
+}
+.discussions h2 a:hover {
+  text-decoration: underline;
+}
+</style>
 <template>
-  <section>
+  <section class="discussions">
     <h1>Discussions</h1>
-    <ol class="discussions" v-if="data">
-      <li v-for="discussion in data.discussions" :key="discussion.id">
-        <NuxtLink :href="`/discussions/${discussion.number}`">{{
-          discussion.title
-        }}</NuxtLink>
+    <div v-if="data">
+      <div v-for="discussion in data.discussions" :key="discussion.id">
+        <h2>
+          <NuxtLink :href="`/discussions/${discussion.number}`">{{
+            discussion.title
+          }}</NuxtLink>
+        </h2>
         <p>
           {{ discussion.author }} started
           <relative-time :ts="discussion.createdAt" />
         </p>
-      </li>
-    </ol>
+      </div>
+    </div>
   </section>
 </template>
