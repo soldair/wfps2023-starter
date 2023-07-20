45698f7c8637cc385aec195cddf1a986b76bdd80
Samuel Arcidiacono <djjeck@google.com>
2023-07-20 15:52:40 +0000

    Style nav

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,6 +1,38 @@
 <style>
-nav .router-link-active {
+body {
+  margin: 0;
+  padding: 0;
+
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+nav {
+  display: block;
+  padding: 25px 20px;
+  box-shadow: 0 5px 5px rgba(0, 0, 0, .3);
+  font-size: 20px;
+}
+
+nav ul {
+  margin: 0;
+  padding: 0;
+}
+
+nav ul li {
+  display: inline-block;
+  margin: 0 15px 0 0;
+}
+
+nav ul li a {
+  text-decoration: none;
+}
+
+nav ul li a.router-link-active {
   font-weight: bold;
+}
+
+main {
+  padding: 20px;
 }
 </style>
 <template>
